6ce9770fc2ff577ea5bb79e0c95efd2e17d68f8d
Alviannn <alvianndq@gmail.com>
2023-01-10 22:51:41 -0800

    feat: add service testing

--- AniDebe/ViewController.swift
+++ AniDebe/ViewController.swift
@@ -11,6 +11,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let baseUrl = "https://api.jikan.moe/v4"
+        let sessionService = SessionService(baseUrl: "\(baseUrl)/seasons")
+        
+        sessionService.currentSeason(page: 1, onCompleteHandler: { data, err in
+            if let err = err {
+                print(err)
+                return
+            }
+            
+            let animeList = data!.data
+            var count = 0
+            
+            for anime in animeList {
+                count += 1
+                print("\(count). \(anime.title) - \(anime.score ?? 0.00)")
+            }
+        })
+        
         // Do any additional setup after loading the view.
     }
 
